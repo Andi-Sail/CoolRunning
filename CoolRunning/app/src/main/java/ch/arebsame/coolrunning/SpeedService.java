ecb61a3c44e968750c2f238fae1094fda4e33ae5
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2019-11-16 15:39:38 -0500

    improve speed service with location provider

--- CoolRunning/app/src/main/java/ch/arebsame/coolrunning/SpeedService.java
+++ CoolRunning/app/src/main/java/ch/arebsame/coolrunning/SpeedService.java
@@ -5,12 +5,15 @@ import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.core.app.ActivityCompat;
 
@@ -18,6 +21,7 @@ public class SpeedService extends Service {
 
     float speed = 0;
     protected LocationManager locationManager;
+    protected LocationListener listener;
     // The minimum distance to change Updates in meters
     private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
     // The minimum time between updates in milliseconds
@@ -29,55 +33,73 @@ public class SpeedService extends Service {
     @Override
     public void onCreate() {
         locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
-        LocationListener listener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                if (location != null) {
-                    speed = location.getSpeed();
-                    //((MainActivity)getBaseContext()).setSpeed(speed);
+        LocationProvider provider = locationManager.getProvider(LocationManager.GPS_PROVIDER);
+        // Retrieve a list of location providers that have fine accuracy, no monetary cost, etc
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setSpeedAccuracy(Criteria.ACCURACY_HIGH);
+        criteria.setSpeedRequired(true);
+        criteria.setCostAllowed(false);
+
+        String providerName = locationManager.getBestProvider(criteria, true);
+
+
+        final boolean gpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+        if (gpsEnabled && providerName != null) {
+            listener = new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        speed = location.getSpeed();
+                    }
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) this.getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                } else {
+                    ActivityCompat.requestPermissions((Activity) this.getBaseContext(),
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            1);
                 }
             }
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) this.getBaseContext(), Manifest.permission.ACCESS_COARSE_LOCATION)) {
 
-            @Override
-            public void onStatusChanged(String provider, int status, Bundle extras) {
-
+                } else {
+                    ActivityCompat.requestPermissions((Activity) this.getBaseContext(),
+                            new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                            1);
+                }
             }
-
-            @Override
-            public void onProviderEnabled(String provider) {
-
-            }
-
-            @Override
-            public void onProviderDisabled(String provider) {
-
-            }
-        };
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) this.getBaseContext(),Manifest.permission.ACCESS_FINE_LOCATION)) {
-
-            } else {
-                ActivityCompat.requestPermissions((Activity) this.getBaseContext(),
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        1);
-            }
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, listener);
         }
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) this.getBaseContext(),Manifest.permission.ACCESS_COARSE_LOCATION)) {
-
-            } else {
-                ActivityCompat.requestPermissions((Activity) this.getBaseContext(),
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        1);
-            }
+        else {
+            Toast.makeText(getBaseContext() ,"GPS is not enabled, please enable and try again", Toast.LENGTH_LONG).show();
         }
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, listener);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        locationManager.removeUpdates(listener);
     }
 
     @Override
