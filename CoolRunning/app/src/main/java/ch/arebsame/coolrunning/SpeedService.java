37c1fc3459d020b6b089ddd9bec1c50428cfc1aa
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2019-11-16 15:18:56 -0500

    add easy implementation to get speed

--- CoolRunning/app/src/main/java/ch/arebsame/coolrunning/SpeedService.java
+++ CoolRunning/app/src/main/java/ch/arebsame/coolrunning/SpeedService.java
@@ -1,11 +1,83 @@
 package ch.arebsame.coolrunning;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.Service;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.os.IBinder;
+import android.widget.TextView;
+
+import androidx.core.app.ActivityCompat;
 
 public class SpeedService extends Service {
+
+    float speed = 0;
+    protected LocationManager locationManager;
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 100;
+
     public SpeedService() {
+    }
+
+    @Override
+    public void onCreate() {
+        locationManager = (LocationManager) this.getSystemService(LOCATION_SERVICE);
+        LocationListener listener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (location != null) {
+                    speed = location.getSpeed();
+                    //((MainActivity)getBaseContext()).setSpeed(speed);
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) this.getBaseContext(),Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions((Activity) this.getBaseContext(),
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        1);
+            }
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale((Activity) this.getBaseContext(),Manifest.permission.ACCESS_COARSE_LOCATION)) {
+
+            } else {
+                ActivityCompat.requestPermissions((Activity) this.getBaseContext(),
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        1);
+            }
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, MIN_TIME_BW_UPDATES, MIN_DISTANCE_CHANGE_FOR_UPDATES, listener);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 
     @Override
