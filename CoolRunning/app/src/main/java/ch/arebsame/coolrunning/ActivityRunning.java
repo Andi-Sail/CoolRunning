3ca050af3d8ba09b4b7974de46fd75ce4975c360
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2019-11-20 15:47:37 -0500

    add base for UI

---
+++ CoolRunning/app/src/main/java/ch/arebsame/coolrunning/ActivityRunning.java
@@ -0,0 +1,17 @@
+package ch.arebsame.coolrunning;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ActivityRunning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_running);
+
+        setTitle("CNIT355 Running App");
+
+    }
+}
--- CoolRunning/app/src/main/java/ch/arebsame/coolrunning/MainActivity.java
+++ CoolRunning/app/src/main/java/ch/arebsame/coolrunning/MainActivity.java
@@ -3,6 +3,8 @@ package ch.arebsame.coolrunning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setTitle("CNIT355 Running App");
+
+        Spinner dropdown = findViewById(R.id.programSpinner);
+        String[] items = new String[]{"Interval", "Increasing Speed", "Constant Speed", "Random Speed"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, items);
+        dropdown.setAdapter(adapter);
+
     }
 }
