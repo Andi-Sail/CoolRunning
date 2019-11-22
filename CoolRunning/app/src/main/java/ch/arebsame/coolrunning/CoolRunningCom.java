6513055c6448f8413c4e0c8eac6b4e8d1a3d975b
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2019-11-22 12:53:26 -0500

    add average speed to CoolRunningCom

--- CoolRunning/app/src/main/java/ch/arebsame/coolrunning/CoolRunningCom.java
+++ CoolRunning/app/src/main/java/ch/arebsame/coolrunning/CoolRunningCom.java
@@ -6,10 +6,18 @@ package ch.arebsame.coolrunning;
  */
 public class CoolRunningCom {
     private static float speed;
+    private static float averageSpeed;
+    private static final int speedDelayLineLength = 10;
+    private static int speedDelayLineIndex = 0;
+    private static float speedDelayLine[] = new float[speedDelayLineLength];
     private static float targetSpeed;
     private static long runningTimeMs;
     private static RunningMode mode;
     private static State state;
+
+    public static float getAverageSpeed() {
+        return averageSpeed;
+    }
 
     public synchronized static float getSpeed() {
         return speed;
@@ -17,6 +25,15 @@ public class CoolRunningCom {
 
     public synchronized static void setSpeed(float speed) {
         CoolRunningCom.speed = speed;
+        speedDelayLine[speedDelayLineIndex] = speed;
+        if (speedDelayLineIndex >= speedDelayLineLength) {
+            speedDelayLineIndex = 0;
+        }
+        float sum = 0;
+        for (float s : speedDelayLine) {
+            sum += s;
+        }
+        averageSpeed = sum / speedDelayLineLength;
     }
 
     public synchronized static float getTargetSpeed() {
