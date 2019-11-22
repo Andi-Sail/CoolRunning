20d298733d0795082a274629c80679323e0b0d2d
Andreas Rebsamen <andreas.rebsamen@bluewin.ch>
2019-11-22 11:58:03 -0500

    add sound effects for speed up and slow down

---
+++ sound/slowDown.rb
@@ -0,0 +1,14 @@
+# Welcome to Sonic Pi v3.1
+play 76.5
+sleep 0.3
+play 60
+sleep 0.4
+play 76.5
+sleep 0.3
+play 60
+sleep 0.4
+play 76.5
+sleep 0.3
+play 60
+sleep 0.4
+
---
+++ sound/speedUp.rb
@@ -0,0 +1,14 @@
+# Welcome to Sonic Pi v3.1
+play 75
+sleep 0.3
+play 90
+sleep 0.4
+play 75
+sleep 0.3
+play 90
+sleep 0.4
+play 75
+sleep 0.3
+play 90
+sleep 0.4
+
